5d0c24d88fd4719af91f24fc2c1b291a25b31411
Lubo Viluda <lubo.viluda@gmail.com>
2020-03-26 10:20:14 +0100

    linear regression for one variable, cost function and gradient descent
    linear regression for one variable, cost function and gradient descent (exercise 1 - week 2), mandatory part

---
+++ ex-1/computeCost.m
@@ -0,0 +1,32 @@
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+
+% You need to return the following variables correctly 
+J = 0;
+
+m = length(y); % number of training examples
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+
+% for loop version
+%for i = 1:m
+%    T = X (i, :);
+ %   R = y (i, 1);
+ %   diff = (T * theta) - R;
+%    J = J + diff.^2;
+%endfor
+%J = (1/(2*m)) * J ; 
+
+% vectorication
+error = (X * theta) - y;
+error_square = error.^2;
+J = (1 / (2 * m)) * sum(error_square);
+
+% =========================================================================
+
+end
