5ac46d01ae8c2c1588975025621163ffb98eb5a9
Lubo Viluda <lubo.viluda@gmail.com>
2020-05-02 19:37:51 +0200

    gaussian kernel and optimal params

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -9,26 +9,35 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
+%all_C = [ 0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30 ];
+%all_sigma = [ 0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30 ];
 
+%smallest_error = 1;
 
+%for i = 1:8
+%  temp_sigma = all_sigma(i,1);
+%  for o = 1:8
+%    fprintf('looping for:');
+%    i 
+%    o 
+    
+%    temp_C = all_C(o,1);
+%    model= svmTrain(X, y, temp_C, @(x1, x2) gaussianKernel(x1, x2, temp_sigma));
+%    predictions = svmPredict(model, Xval);
+%    current_error = mean(double(predictions ~= yval))
+%    smallest_error
+%    if (smallest_error > current_error)
+%      fprintf('found new better params, prediction: \n');
+%      smallest_error = current_error;
+%      C = temp_C;
+%      sigma = temp_sigma;
+%   endif
+%  endfor
+%endfor
 
-
-
-
-
-% =========================================================================
+%C
+%sigma
 
 end
