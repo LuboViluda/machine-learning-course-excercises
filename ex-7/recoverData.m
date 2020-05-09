d214be59a2911f19a6612cedf8e9bac5fec01a30
Lubo Viluda <lubo.viluda@gmail.com>
2020-05-09 12:21:43 +0200

    remove comments

--- ex-7/recoverData.m
+++ ex-7/recoverData.m
@@ -6,11 +6,8 @@ function X_rec = recoverData(Z, U, K)
 %   approximate reconstruction in X_rec.
 %
 
-% You need to return the following variables correctly.
 X_rec = zeros(size(Z, 1), size(U, 1));
 
 X_rec = Z * U(:,1:K)';
-
-% =============================================================
 
 end
